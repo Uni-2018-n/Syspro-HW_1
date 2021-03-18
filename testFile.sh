004f85d2339727f0d78f4e1019d98321d37bcfdb
smackflad <ask.kalamakhs@hotmail.com>
2021-03-18 14:11:20 +0200

    done with arguments fixed some things in duplicates

--- testFile.sh
+++ testFile.sh
@@ -2,17 +2,15 @@
 if [ $# -ne 4 ]
 then
   echo Wrong Input
-  # exit 1
+  exit 1
 fi
 #$(( (( `date+%N` / 1000) % BASE)+1))
 OUTFILE=input.txt;
-db=1;
 touch "${OUTFILE}"; rm "${OUTFILE}"; touch "${OUTFILE}";
-test=10000;
-c=( `shuf -n $test Data/countries.txt` ) #array of countries
-v=( `shuf -n $test Data/viruses.txt` ) #array of viruses
+c=( `shuf -r -n$3 $2` ) #array of countries
+v=( `shuf -r -n$3 $1` ) #array of viruses
 i=0;
-while [ $i -lt "$test" ]
+while [ $i -lt "$3" ]
   do
     t=`expr $RANDOM % 9999`; #id
     let id=t+1;
@@ -20,22 +18,22 @@ while [ $i -lt "$test" ]
 
     t=`expr $RANDOM % 12`;
     let nam=t+3;
-    fn=`shuf -zer -n$nam {a..z} | tr -d '\0'`
+    fn=`shuf -zer -r -n$nam {a..z} | tr -d '\0'`
     echo -n "${fn} "
 
     t=`expr $RANDOM % 12`;
     let nam=t+3;
-    ln=`shuf -zer -n$nam {a..z} | tr -d '\0'`
+    ln=`shuf -zer -r -n$nam {a..z} | tr -d '\0'`
     echo -n "${ln} "
 
-    cc=${c[`expr $i % 111`]};
+    cc=${c[$i]};
     echo -n "${cc} "
 
     t=`expr $RANDOM % 120`; #age
     let a=t+1;
     echo -n "${a} "
 
-    echo -n "${v[`expr $i % 141`]} "
+    echo -n "${v[$i]} "
 
     t=`expr $RANDOM % 100`;
     let tmp=t+1;
@@ -56,16 +54,14 @@ while [ $i -lt "$test" ]
     echo -n "${vacced} "
     echo
     i=$(($i+1))
-    if [ ${db} -eq 1 ] && [ $i -lt "$test" ]
+    if [ $4 -eq 1 ] && [ $i -lt "$3" ]
     then
       t=`expr $RANDOM % 100`;
       let p=t+1;
-      while [ $p -lt 20 ] && [ $i -lt "$test" ]
+      while [ $p -lt 20 ] && [ $i -lt "$3" ]
       do
         echo -n "${id} ${fn} ${ln} ${cc} ${a} "
-        t=`expr $RANDOM % 111` #TODO use len of file
-        let dup=t+1;
-        echo -n "${v[$dup]} "
+        echo -n "${v[$i]} "
 
         t=`expr $RANDOM % 100`;
         let tmp=t+1;
